ee6bb544f3c6f14c5ff8fef96a63d6c8f65033b1
wcoqui <wcoqui@gmail.com>
2025-03-29 21:36:41 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdvancedPromptGenerator from './components/AdvancedPromptGenerator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-6">
+      <AdvancedPromptGenerator />
+    </div>
+  );
+}
+
+export default App;
